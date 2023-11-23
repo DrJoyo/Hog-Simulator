fb86726eb2a0a72a09feb38ffd64c7a8f901d209
Johan Ko <drjoyo@gmail.com>
2023-11-23 15:41:35 -0800

    added parameter for filename to read/write, added documentation to code

--- HogSimulator/optimizer.py
+++ HogSimulator/optimizer.py
@@ -2,7 +2,7 @@ from math import comb
 from decimal import *
 import sys
 
-
+# Calculates the probability distribution of the score from rolling n dice using generating functions
 def all_possible(n):
     assert type(n)==int and 1<=n<=10
     probs = [0]*(6*n)
@@ -15,17 +15,20 @@ def all_possible(n):
         probs[(2*n-1)+i] = i_prob
     return probs
 
+# Returns the next perfect square if n is a perfect square, or just n otherwise
 def next_square(n):
     assert type(n) == int
-    squart = n**0.5
-    if (squart%1)==0:
-        return int((squart+1)**2)
+    sqrt = n**0.5
+    if (sqrt%1)==0:
+        return int((sqrt+1)**2)
     else:
         return n
 
-def tail_points(opponent_score):
+# Calculates the number of points scored by rolling 0 dice given the opponent's score
+def zero_dice_points(opponent_score):
     return 2*abs((opponent_score//10)%10 - opponent_score%10)+1
 
+# Functions to access data in interactive mode, for debugging or finding optimal play in game
 def p(x, y, accuracy = 2):
     print('Win chance: ' + str(round(prob_grid[x][y]*100, accuracy)) + '%')
 def m(x, y):
@@ -34,67 +37,74 @@ def pm(x, y, accuracy = 2):
     print('Win chance: ' + str(round(prob_grid[x][y]*100, accuracy)) + '%')
     print('Best move: ' + str(move_grid[x][y]))
 
+# Set default values
 win_score = 100
 max_dice = 10
 precision = 20
+write_file = 'opt_moves.txt'
 
+# Take arguments
 if (len(sys.argv) > 1):
     win_score = int(sys.argv[1])
 if (len(sys.argv) > 2):
     max_dice = int(sys.argv[2])
 if (len(sys.argv) > 3):
     precision = int(sys.argv[3])
+if (len(sys.argv) > 4):
+    write_file = sys.argv[4]
 
+# Set precision of Decimal calculations
 getcontext().prec = precision
 
+# Calculate probability distributions of score for all legal numbers of rolls
 poss_list = [all_possible(i) for i in range(1, max_dice + 1)]
 
-prob_grid = []
-for i in range(win_score):
-    prob_grid.append([0]*win_score)
+# Create arrays for dynamic programming
+prob_grid = [[0]*win_score for i in range(win_score)]
 move_grid = [[0]*win_score for i in range(win_score)]
 
-
+# Base case
 prob_grid[win_score - 1][win_score - 1] = Decimal(1)
+move_grid[win_score - 1][win_score - 1] = 0
 
+# Recursive algorithm, called on diagonals 
 for i in range(2, 2*win_score):
     for j in range(max(0, i-win_score), min(win_score, i)):
-        x = win_score-1-((i-1)-j) #player score
-        y = win_score-1-j #opponent score
+        x = win_score-1-((i-1)-j) # player score
+        y = win_score-1-j # opponent score
         done = False
-        current_max = Decimal(0)
-        current_rolls = 0
-        if next_square(x + tail_points(y)) >= win_score: #returns 1 if score goes over win_score
-            current_max = 1
+        current_max_prob = Decimal(0)
+        current_max_rolls = 0
+        if next_square(x + zero_dice_points(y)) >= win_score: # returns probability 1 if score goes over win_score
+            current_max_prob = 1
             done = True
         else:
-            current_max = 1 - prob_grid[y][next_square(x + tail_points(y))] #prob from 0 rolls
-        if current_max==1:
+            current_max_prob = 1 - prob_grid[y][next_square(x + zero_dice_points(y))] # probability of winning after 0 rolls
+        if current_max_prob==1:
             done = True
-        r = 0
-        while r < max_dice and done == False:
+        roll_num = 1 # Number of rolls
+        while roll_num <= max_dice and done == False: # Compare expected probability of winning after all positive dice roll #'s
             total_prob = Decimal(0)
             if next_square(x+1) >= win_score:
-                total_prob += 1 * poss_list[r][0]
+                total_prob += 1 * poss_list[roll_num-1][0]
             else:
-                total_prob += (1-prob_grid[y][next_square(x+1)]) * poss_list[r][0]
-            for roll in range(2*(r+1), 6*(r+1)+1): #remember to subtract 1 for indices
-                if next_square(x + roll) >= win_score:
-                    total_prob += 1 * poss_list[r][roll-1]
+                total_prob += (1-prob_grid[y][next_square(x+1)]) * poss_list[roll_num-1][0]
+            for score in range(2*roll_num, 6*roll_num+1): # For all possible scores after rolling
+                if next_square(x + score) >= win_score:
+                    total_prob += 1 * poss_list[roll_num-1][score-1]
                 else: 
-                    total_prob += (1 - prob_grid[y][next_square(x + roll)]) * poss_list[r][roll-1]
-            if total_prob > current_max:
-                current_max = total_prob
-                current_rolls = r+1
-            if current_max == 1:
+                    total_prob += (1 - prob_grid[y][next_square(x + score)]) * poss_list[roll_num-1][score-1]
+            if total_prob > current_max_prob: # Compare expected probability to current best
+                current_max_prob = total_prob
+                current_max_rolls = roll_num
+            if current_max_prob == 1: # Exit early if expected probability is 1
                 done = True
-            r += 1
-        prob_grid[x][y] = current_max
-        move_grid[x][y] = current_rolls
+            roll_num += 1
+        prob_grid[x][y] = current_max_prob
+        move_grid[x][y] = current_max_rolls
 
-f = open('opt_moves.txt', 'w')
+# Write optimal moves to the specified text file (opt_moves.txt by default)
+f = open(write_file, 'w')
 f.writelines([str(lst)[1:-1] + '\n' for lst in move_grid])
 f.close()
-#print(prob_grid)
-#print(move_grid)
 
--- HogSimulator/src/HogSimulator.java
+++ HogSimulator/src/HogSimulator.java
@@ -7,53 +7,61 @@ public class HogSimulator {
     private int winScore;
     private int maxDice;
     private Random random;
+    private String fileName;
     private int[][] optimalMoves;
     private BufferedReader stdIn;
     public static void main(String[] args) throws IOException {
         HogSimulator game;
         switch (args.length) {
             case 0:
-                game = new HogSimulator(100, 10, 1);
+                game = new HogSimulator(100, 10, 1, "opt_moves.txt");
                 break;
             case 1:
-                game = new HogSimulator(Integer.parseInt(args[0]), 10, 1);
+                game = new HogSimulator(Integer.parseInt(args[0]), 10, 1, "opt_moves.txt");
                 break;
             case 2:
-                game = new HogSimulator(Integer.parseInt(args[0]), Integer.parseInt(args[1]), 1);
+                game = new HogSimulator(Integer.parseInt(args[0]), Integer.parseInt(args[1]), 1, "opt_moves.txt");
+                break;
+            case 3:
+                game = new HogSimulator(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), "opt_moves.txt");
                 break;
             default:
-                game = new HogSimulator(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+                game = new HogSimulator(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]), args[3]);
+                break;
         }
         game.runGame();
     }
-    public HogSimulator(int winScore, int maxDice, int type) throws IOException {
+    public HogSimulator(int winScore, int maxDice, int type, String fileName) throws IOException {
         this.type = type;
         this.winScore = winScore;
         this.maxDice = maxDice;
         this.random = new Random();
         this.stdIn = new BufferedReader(new InputStreamReader(System.in));
+        this.fileName = fileName;
         if (type > 0) {
             this.optimalMoves = readOptimalMoves();
         }
     }
+    /** Reads optimal moves from the specified file */
     public int[][] readOptimalMoves() throws IOException {
         int[][] toReturn = new int[winScore][winScore];
-        BufferedReader fileReader = new BufferedReader(new FileReader("opt_moves.txt"));
+        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
         for (int i = 0; i < winScore; i++) {
             String line = fileReader.readLine();
             String[] split = line.split(", ");
             if (split.length != winScore) {
-                throw new IllegalStateException("opt_moves.txt dimensions do not match input");
+                throw new IllegalStateException(fileName + " dimensions do not match input");
             }
             for (int j = 0; j < winScore; j++) {
                 toReturn[i][j] = Integer.parseInt(split[j]);
             }
         }
         if (fileReader.readLine() != null) {
-            throw new IllegalStateException("opt_moves.txt dimensions do not match input");
+            throw new IllegalStateException(fileName + " dimensions do not match input");
         }
         return toReturn;
     }
+    /** Simulates a game between two players */
     public void runGame() throws IOException {
         int turn = 0;
         int score0 = 0;
@@ -80,6 +88,8 @@ public class HogSimulator {
             }
         }
     }
+
+    /** Tells the player to type in a number or returns the computer's optimal play */
     public int promptInput(int myScore, int otherScore, int isComputer, int playerNum) throws IOException {
         if (isComputer == 0) {
             System.out.println("Player " + playerNum + ", the score is " + myScore + " to " + otherScore + ". How many dice would you like to roll? (0-" + maxDice + ")");
@@ -104,20 +114,7 @@ public class HogSimulator {
             return optimalMoves[myScore][otherScore];
         }
     }
-    public int[] rollDice(int numRolls) {
-        int[] toReturn = new int[numRolls];
-        for (int i = 0; i < numRolls; i++) {
-            toReturn[i] = random.nextInt(1, 7);
-        }
-        return toReturn;
-    }
-    public int zeroRoll(int otherScore) {
-        return 2 * Math.abs((otherScore / 10) % 10 - otherScore % 10) + 1;
-    }
-    public boolean isPerfectSquare(int num) {
-        int sqrt = (int) Math.sqrt(num);
-        return (sqrt * sqrt == num);
-    }
+    /** Computes the new score after rolling numRolls times */
     public int computeScore(int numRolls, int myScore, int otherScore, int playerNum) {
         int toAdd;
         if (numRolls == 0) {
@@ -149,6 +146,24 @@ public class HogSimulator {
             System.out.println("Player " + playerNum + "'s score is now " + myScore + " + " + toAdd + " = " + (myScore + toAdd) + "!");
             return myScore + toAdd;
         }
-
     }
+
+    /** Simulates numRolls dice rolls and returns the array of rolls */
+    public int[] rollDice(int numRolls) {
+        int[] toReturn = new int[numRolls];
+        for (int i = 0; i < numRolls; i++) {
+            toReturn[i] = random.nextInt(1, 7);
+        }
+        return toReturn;
+    }
+    /** Returns score from rolling zero dice */
+    public int zeroRoll(int otherScore) {
+        return 2 * Math.abs((otherScore / 10) % 10 - otherScore % 10) + 1;
+    }
+    /** Checks if a number is a perfect square */
+    public boolean isPerfectSquare(int num) {
+        int sqrt = (int) Math.sqrt(num);
+        return (sqrt * sqrt == num);
+    }
+
 }
